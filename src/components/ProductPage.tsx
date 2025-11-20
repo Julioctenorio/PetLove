5ce614c58ffabbe476fa72b2196308a0795086e0
mrtbrt <martajbrito@gmail.com>
2025-11-20 13:15:28 +0000

    update productpage

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "lucide-react"
+import { ArrowLeft, Heart } from "lucide-react"
 import Shampoo from "../images/Shampoo.png"
 
 
@@ -8,22 +8,33 @@ export default function ProductPage() {
 
 
 
-        <div className="border border-black bg-white w-[393px] h-[593px]">
-            <div>
-              <ArrowLeft />  
-       
+        <div className="flex flex-col border border-black bg-white w-[393px] h-fit">
+            <div className="p-3.5">
+                <ArrowLeft />
             </div>
+
+            <h1 className="self-center font-bold text-2xl">Shampoo Neutro Sanol</h1>
 
 
             <div className="p-4 w-[393px] h-[393px]">
                 <img src="/images/Shampoo.png"
-                    alt="shampoo neutro para caes e gatos">
-
-
-
-                </img>
+                    alt="shampoo neutro para caes e gatos" />
 
             </div>
+            <div className="self-end p-7">
+                <Heart className="text-black hover:text-red-500 " />
+            </div>
+
+<div className="flex gap-2 m-2">
+
+    <p>4,9</p>
+    <Heart />
+    <p>500+</p>
+</div>
+<div className="w-full flex justify-between">
+    <p>2499€</p>
+    <p>10 em stock</p>
+</div>
 
 
         </div>
