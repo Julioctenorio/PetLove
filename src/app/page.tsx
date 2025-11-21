566937de132cc46b7e904859dfcd5d94dacebdff
mrtbrt <martajbrito@gmail.com>
2025-11-21 10:52:33 +0000

    add cardquality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Button from "@/components/Button";
 import CardAnimal from "@/components/CardAnimal";
 import CardCategory from "@/components/CardCategory";
+import CardQuality from "@/components/CardQuality";
 import ProductPage from "@/components/ProductPage";
 import Image from "next/image";
 
@@ -15,6 +16,7 @@ export default function Home() {
   <CardAnimal/>
  <ProductPage/>
   <CardCategory/>
+  <CardQuality/>
 </div>
 <div>
 
